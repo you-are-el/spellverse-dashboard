5ab41cbe6a06436d989a3b900618def958c6c062
you-are-el <you_are_el@protonmail.com>
2024-11-19 18:07:33 +0100

    switch to live mode

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from collections import Counter
 import subprocess
 
 # Development mode flag
-DEV_MODE = True
+DEV_MODE = False
 
 # Page config
 st.set_page_config(page_title="Spellverse Tracker", layout="wide")
